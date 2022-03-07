cf4a5eee009a0464277a0e6b8451b5c3b42410e8
lfoliveir4 <luis.felipi@hotmail.com>
2022-03-07 20:56:29 -0300

    remove folder helpers not used

--- src/helpers/getUrlSignInWithGoogle.ts
+++
@@ -1,8 +0,0 @@
-export function getUrlSignInWithGoogle(
-  clientId: string,
-  redirectUri: string,
-  responseType: string,
-  scope: string
-): string {
-  return `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`
-}
